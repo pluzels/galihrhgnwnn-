91ab84d2a14cf81892fecbf668846cec03fef651
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-16 23:48:40 +0700

    Update playaudio.js

--- function/scraper/playaudio.js
+++ function/scraper/playaudio.js
@@ -1,53 +1,52 @@
-const yt = require('ytdl-core');
+const ytdl = require('@distube/ytdl-core');
 const yts = require('yt-search');
 
 async function playaudio(query) {
-    return new Promise((resolve, reject) => {
-        try {
-            const search = yts(query)
+  return new Promise((resolve, reject) => {
+    try {
+      yts(query)
+        .then((data) => {
+          const url = [];
+          const pormat = data.all;
+          for (let i = 0; i < pormat.length; i++) {
+            if (pormat[i].type === 'video') {
+              let dapet = pormat[i];
+              url.push(dapet.url);
+            }
+          }
+          const id = ytdl.getVideoID(url[0]);
+          ytdl.getInfo(`https://www.youtube.com/watch?v=${id}`)
             .then((data) => {
-                const url = [];
-                const pormat = data.all;
-                for (let i = 0; i < pormat.length; i++) {
-                    if (pormat[i].type == 'video') {
-                        let dapet = pormat[i];
-                        url.push(dapet.url);
-                    }
+              let pormat = data.formats;
+              let audio = [];
+              for (let i = 0; i < pormat.length; i++) {
+                if (pormat[i].mimeType === 'audio/webm; codecs="opus"') {
+                  let aud = pormat[i];
+                  audio.push(aud.url);
                 }
-                const id = yt.getVideoID(url[0]);
-                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
-                .then((data) => {
-                    let pormat = data.formats;
-                    let audio = [];
-                    for (let i = 0; i < pormat.length; i++) {
-                        if (pormat[i].mimeType == 'audio/webm; codecs="opus"') {
-                            let aud = pormat[i];
-                            audio.push(aud.url);
-                        }
-                    }
-                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText;
-                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName;
-                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount;
-                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate;
-                    const result = {
-                        title: title,
-                        thumb: thumb,
-                        channel: channel,
-                        published: published,
-                        views: views,
-                        url: audio[0]
-                    };
-                    return(result);
-                });
-                return(yutub);
-            });
-            resolve(search);
-        } catch (error) {
-            reject(error);
-        }
-        console.log(error);
-    });
+              }
+              const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText;
+              const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
+              const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName;
+              const views = data.player_response.microformat.playerMicroformatRenderer.viewCount;
+              const published = data.player_response.microformat.playerMicroformatRenderer.publishDate;
+              const result = {
+                title: title,
+                thumb: thumb,
+                channel: channel,
+                published: published,
+                views: views,
+                url: audio[0]
+              };
+              resolve(result);
+            })
+            .catch(reject);
+        })
+        .catch(reject);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 module.exports = playaudio;
