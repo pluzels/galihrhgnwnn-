5d7975788abe737974996ce90e366a6061674524
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-18 14:43:05 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    playaudio: require('./scraper/playaudio'),
+    ytaudiodl: require('./scraper/ytaudiodl'),
     blackboxAIChat: require('./scraper/blackboxAIChat'),
     smartContract: require('./scraper/smartContract'),
     ragBot: require('./scraper/ragBot'),
