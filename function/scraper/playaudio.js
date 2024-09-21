0a77337772fcd6838d73f2d8b3076e6cb97fb2f8
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-21 20:49:58 +0700

    Update playaudio.js

--- function/scraper/playaudio.js
+++ function/scraper/playaudio.js
@@ -4,7 +4,7 @@ const ytdl = require('@distube/ytdl-core');
 async function playaudio(query) {
   return new Promise((resolve, reject) => {
     try {
-      yts(query).then(data => {
+      yts(query).then(async data => {
         const video = data.videos[0];
         if (!video) {
           return reject(new Error('Video not found'));
@@ -21,27 +21,26 @@ async function playaudio(query) {
           }
         };
 
-        ytdl(videoUrl, options).then(info => {
-          const formats = info.formats;
-          const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm'));
+        const info = await ytdl.getInfo(videoUrl, options);
+        const formats = info.formats;
+        const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm'));
 
-          if (!audioFormats.length) {
-            return reject(new Error('No audio formats found'));
-          }
+        if (!audioFormats.length) {
+          return reject(new Error('No audio formats found'));
+        }
 
-          const bestAudio = audioFormats[0];
+        const bestAudio = audioFormats[0];
 
-          const result = {
-            title: video.title,
-            thumb: video.thumbnail,
-            channel: video.author.name,
-            published: video.ago,
-            views: video.views,
-            url: bestAudio.url
-          };
+        const result = {
+          title: video.title,
+          thumb: video.thumbnail,
+          channel: video.author.name,
+          published: video.ago,
+          views: video.views,
+          url: bestAudio.url
+        };
 
-          resolve(result);
-        }).catch(reject);
+        resolve(result);
       }).catch(reject);
     } catch (error) {
       reject(error);
