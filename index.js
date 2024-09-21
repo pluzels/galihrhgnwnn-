03aff699b9a57967f2e8a4573430dc7002a68fb7
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-22 06:21:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var express = require("express"),
-  cors = require("cors"),
-  secure = require("ssl-express-www");
-const path = require("path");
-const os = require("os");
-const fs = require("fs");
-const ptz = require("./function/index");
-const axios = require("axios");
+var express = require("express"), cors = require("cors"), secure = require("ssl-express-www");
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const ptz = require('./function/index') 
+const axios = require('axios')
 
 var app = express();
 app.enable("trust proxy");
 app.set("json spaces", 2);
 app.use(cors());
 app.use(secure);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 
-app.get("/stats", (req, res) => {
+app.get('/stats', (req, res) => {
   const stats = {
     platform: os.platform(),
     architecture: os.arch(),
@@ -34,16 +32,16 @@ app.get("/stats", (req, res) => {
     nodeVersion: process.version,
     execPath: process.execPath,
     cwd: process.cwd(),
-    memoryUsage: process.memoryUsage(),
+    memoryUsage: process.memoryUsage()
   };
   res.json(stats);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 
-app.get("/api/ragbot", async (req, res) => {
+app.get('/api/ragbot', async (req, res) => {
   try {
     const message = req.query.message;
     if (!message) {
@@ -52,8 +50,8 @@ app.get("/api/ragbot", async (req, res) => {
     const response = await ptz.ragBot(message);
     res.status(200).json({
       status: 200,
-      creator: "galihrhgnwn",
-      data: { response },
+      creator: "siputzx",
+      data: { response }
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -61,53 +59,17 @@ app.get("/api/ragbot", async (req, res) => {
 });
 
 // Endpoint untuk degreeGuru
-app.get("/api/degreeguru", async (req, res) => {
+app.get('/api/degreeguru', async (req, res) => {
   try {
-    const { message } = req.query;
+    const { message }= req.query;
     if (!message) {
       return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
     }
     const response = await ptz.degreeGuru(message);
     res.status(200).json({
       status: 200,
-      creator: "galihrhgnwn",
-      data: { response },
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-// Endpoint untuk smartContract
-app.get("/api/smartcontract", async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await ptz.smartContract(message);
-    res.status(200).json({
-      status: 200,
-      creator: "galihrhgnwn",
-      data: { response },
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint untuk blackboxAIChat
-app.get("/api/blackboxAIChat", async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
-    }
-    const response = await ptz.blackboxAIChat(message);
-    res.status(200).json({
-      status: 200,
-      creator: "galihrhgnwn",
-      data: { response },
+      creator: "siputzx",
+      data: { response }
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -153,39 +115,76 @@ app.get("/api/playmusic", async (req, res) => {
   }
 });
 
-app.get("/api/gpt", async (req, res) => {
-  const text = req.query.text;
-
-  if (!text) {
-    return res.status(400).send("Parameter 'text' is required.");
-  }
-
+// Endpoint untuk smartContract
+app.get('/api/smartcontract', async (req, res) => {
   try {
-    const requestData = {
-      operation: "chatExecute",
-      params: {
-        text: text,
-        languageId: "6094f9b4addddd000c04c94b",
-        toneId: "60572a649bdd4272b8fe358c",
-        voiceId: "",
-      },
-    };
-
-    const config = {
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        Authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
-        "Content-Type": "application/json",
-      },
-    };
-    let { data } = await axios.post("https://api.rytr.me/", requestData, config);
-    data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, "");
-    res.json(data);
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.smartContract(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ error: error.message });
   }
 });
+
+// Endpoint untuk blackboxAIChat
+app.get('/api/blackboxAIChat', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ptz.blackboxAIChat(message);
+    res.status(200).json({
+      status: 200,
+      creator: "siputzx",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/gpt", async (req, res) => {
+const text = req.query.text;
+
+if (!text) {
+return res.status(400).send("Parameter 'text' is required.");
+}
+
+try {
+const requestData = {
+operation: "chatExecute",
+params: {
+text: text,
+languageId: "6094f9b4addddd000c04c94b",
+toneId: "60572a649bdd4272b8fe358c",
+voiceId: ""
+}
+};
+
+const config = {
+headers: {
+Accept: "application/json, text/plain, */*",
+Authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
+"Content-Type": "application/json"
+}
+};
+let {data} = await axios.post("https://api.rytr.me/", requestData, config)
+data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, '');
+res.json(data);
+} catch (error) {
+console.error(error);
+res.status(500).send("Internal Server Error");
+}
+});
+
 
 app.use((req, res, next) => {
   res.status(404).send("Halaman tidak ditemukan");
@@ -193,7 +192,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Ada kesalahan pada server");
+  res.status(500).send('Ada kesalahan pada server');
 });
 
 app.listen(port, () => {
