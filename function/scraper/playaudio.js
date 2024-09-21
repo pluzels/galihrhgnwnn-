23dba306616f968732a99d08dc8ba55ee2c060b1
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-21 21:32:28 +0700

    Update playaudio.js

--- function/scraper/playaudio.js
+++ function/scraper/playaudio.js
@@ -1,44 +1,40 @@
-const yts = require('yt-search');
-const ytdl = require('@distube/ytdl-core');
+const yts = require('yt-search'); // yt-search untuk mencari video
+const { ytdl } = require('@distube/ytdl-core'); // ytdl-core untuk mengambil data audio
 
 async function playaudio(query) {
   return new Promise((resolve, reject) => {
-    try {
-      // Cari video menggunakan yt-search
-      yts(query).then(data => {
-        const video = data.videos[0]; // Ambil video pertama dari hasil pencarian
-        if (!video) {
-          return reject(new Error('Video not found'));
+    yts(query).then(data => {
+      const video = data.videos[0]; // Ambil video pertama dari hasil pencarian
+      if (!video) {
+        return reject(new Error('Video not found'));
+      }
+
+      const videoUrl = video.url;
+
+      // Menggunakan ytdl-core untuk mendapatkan info audio
+      ytdl.getInfo(videoUrl).then(info => {
+        const formats = info.formats;
+        const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm')); // Filter audio format
+
+        if (!audioFormats.length) {
+          return reject(new Error('No audio formats found'));
         }
 
-        const videoUrl = video.url;
+        const bestAudio = audioFormats[0]; // Ambil format audio terbaik
 
-        // Menggunakan ytdl-core untuk mendapatkan info audio
-        ytdl.getInfo(videoUrl).then(info => {
-          const formats = info.formats;
-          const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm')); // Filter audio format
+        const result = {
+          title: video.title,
+          thumb: video.thumbnail,
+          channel: video.author.name,
+          published: video.ago,
+          views: video.views,
+          url: bestAudio.url // URL audio
+        };
 
-          if (!audioFormats.length) {
-            return reject(new Error('No audio formats found'));
-          }
-
-          const bestAudio = audioFormats[0]; // Ambil format audio terbaik
-
-          const result = {
-            title: video.title,
-            thumb: video.thumbnail,
-            channel: video.author.name,
-            published: video.ago,
-            views: video.views,
-            url: bestAudio.url // URL audio yang bisa digunakan untuk mendownload
-          };
-
-          resolve(result);
-        }).catch(reject); // Tangani kesalahan saat mengambil info dengan ytdl-core
-      }).catch(reject); // Tangani kesalahan saat pencarian dengan yt-search
-    } catch (error) {
-      reject(error); // Tangani kesalahan lainnya
-    }
+        resolve(result); // Kembalikan hasilnya
+      }).catch(reject);
+    }).catch(reject);
   });
 }
+
 module.exports = playaudio;
