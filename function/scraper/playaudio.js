96da588cad06ca915bdf639cfb990788ccc4fb10
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-21 20:48:39 +0700

    Update playaudio.js

--- function/scraper/playaudio.js
+++ function/scraper/playaudio.js
@@ -4,9 +4,8 @@ const ytdl = require('@distube/ytdl-core');
 async function playaudio(query) {
   return new Promise((resolve, reject) => {
     try {
-      // Cari video menggunakan yt-search
       yts(query).then(data => {
-        const video = data.videos[0]; // Ambil video pertama dari hasil pencarian
+        const video = data.videos[0];
         if (!video) {
           return reject(new Error('Video not found'));
         }
@@ -17,21 +16,20 @@ async function playaudio(query) {
         const options = {
           requestOptions: {
             headers: {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/58.0.3029.110 Safari/537.3'
+              'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36'
             }
           }
         };
 
-        // Mengambil info audio
-        ytdl.getInfo(videoUrl, options).then(info => {
+        ytdl(videoUrl, options).then(info => {
           const formats = info.formats;
-          const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm')); // Filter audio format
+          const audioFormats = formats.filter(format => format.mimeType && format.mimeType.includes('audio/webm'));
 
           if (!audioFormats.length) {
             return reject(new Error('No audio formats found'));
           }
 
-          const bestAudio = audioFormats[0]; // Ambil format audio terbaik
+          const bestAudio = audioFormats[0];
 
           const result = {
             title: video.title,
@@ -39,14 +37,14 @@ async function playaudio(query) {
             channel: video.author.name,
             published: video.ago,
             views: video.views,
-            url: bestAudio.url // URL audio yang bisa digunakan untuk mendownload
+            url: bestAudio.url
           };
 
           resolve(result);
-        }).catch(reject); // Tangani kesalahan saat mengambil info dengan ytdl-core
-      }).catch(reject); // Tangani kesalahan saat pencarian dengan yt-search
+        }).catch(reject);
+      }).catch(reject);
     } catch (error) {
-      reject(error); // Tangani kesalahan lainnya
+      reject(error);
     }
   });
 }
