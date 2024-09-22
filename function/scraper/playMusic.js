3c33382918d909e9d2c9c9d97abeec7467da5372
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-22 22:46:10 +0700

    Update playMusic.js

--- function/scraper/playMusic.js
+++ function/scraper/playMusic.js
@@ -10,16 +10,15 @@ async function playMusic(query) {
         return reject(new Error('lagu tidak ditemukan.'));
       }
 
-      const audioStream = await playdl.stream(video.url);
+      // Ambil URL audio stream dari play-dl
+      const videoInfo = await playdl.video_info(video.url);
+      const audioFormat = playdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
 
       const result = {
+        id: video.videoId,
         title: video.title,
-        thumb: video.thumbnail,
-        channel: video.author.name,
-        published: video.ago,
-        views: video.views,
-        streamUrl: audioStream.stream, // Stream audio
-        type: audioStream.type // 'opus' atau 'mp3'
+        thumbnail: video.thumbnail,
+        audio: audioFormat.url, // Link langsung ke file audio yang bisa di-play
       };
 
       resolve(result);
