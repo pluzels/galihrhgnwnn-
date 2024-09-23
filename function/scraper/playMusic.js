81d454bfb3786f3b2bbea61aa95c104f8d218f0b
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-23 17:23:44 +0700

    Update playMusic.js

--- function/scraper/playMusic.js
+++ function/scraper/playMusic.js
@@ -1,23 +1,24 @@
 const yts = require('yt-search');
 const playdl = require('play-dl');
+const fs = require('fs');
 
 async function playMusic(query) {
   return new Promise(async (resolve, reject) => {
     try {
       const data = await yts(query);
       const video = data.videos[0];
+
       if (!video) {
         return reject(new Error('lagu tidak ditemukan.'));
       }
 
-      // Dapatkan stream audio dari video menggunakan playdl.stream
-      const stream = await playdl.stream(video.url);
+      const audioStream = await playdl.stream(video.url);
 
       const result = {
         id: video.videoId,
         title: video.title,
         thumbnail: video.thumbnail,
-        audio: stream.url, // Link stream langsung
+        audioStream: audioStream.stream // stream audio langsung
       };
 
       resolve(result);
