18388de0fa8890a28c6b4b4f1aeae1099fefabfc
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-23 20:21:55 +0700

    Update playMusic.js

--- function/scraper/playMusic.js
+++ function/scraper/playMusic.js
@@ -1,5 +1,5 @@
 const yts = require('yt-search');
-const yt = require('ytdl-core');
+const youtubedl = require('youtube-dl-exec');
 
 async function playmusic(query) {
   return new Promise(async (resolve, reject) => {
@@ -11,26 +11,21 @@ async function playmusic(query) {
         return reject(new Error('Lagu tidak ditemukan.'));
       }
 
-      // Menggunakan ytdl-core untuk mendapatkan link stream audio
-      const info = await yt.getInfo(video.url);
-
-      // Memilih format dengan audioonly
-      const format = yt.chooseFormat(info.formats, { filter: 'audioonly', quality: 'highestaudio' });
-
-      if (!format || !format.url) {
-        return reject(new Error('Stream audio tidak tersedia.'));
-      }
+      // Menggunakan youtube-dl-exec untuk mendapatkan link stream audio
+      const info = await youtubedl(video.url, {
+        format: 'bestaudio',
+        dumpSingleJson: true
+      });
 
       const result = {
         id: video.videoId,
         title: video.title,
         thumbnail: video.thumbnail,
-        audio: format.url, 
+        audio: info.url,  // Mendapatkan link stream langsung
       };
 
       resolve(result);
     } catch (error) {
-      // Menangani error dan memberikan pesan yang lebih informatif
       console.error('Error in playmusic:', error);
       reject(new Error('Terjadi kesalahan saat mengambil data musik.'));
     }
