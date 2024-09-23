4b67e5226883bd6529fb51a65154badb210dc7bc
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-23 19:15:46 +0700

    Update playMusic.js

--- function/scraper/playMusic.js
+++ function/scraper/playMusic.js
@@ -1,5 +1,5 @@
 const yts = require('yt-search');
-const playdl = require('play-dl');
+const ytdl = require('@distube/ytdl-core');
 
 async function playMusic(query) {
   return new Promise(async (resolve, reject) => {
@@ -7,17 +7,29 @@ async function playMusic(query) {
       const data = await yts(query);
       const video = data.videos[0];
       if (!video) {
-        return reject(new Error('lagu tidak ditemukan.'));
+        return reject(new Error('Lagu tidak ditemukan.'));
       }
 
-      // Dapatkan stream audio dari video menggunakan playdl.stream
-      const stream = await playdl.stream(video.url);
+      // Cek apakah URL video valid
+      if (!ytdl.validateURL(video.url)) {
+        return reject(new Error('URL video tidak valid.'));
+      }
+
+      // Dapatkan stream audio dari video dengan User-Agent khusus
+      const audioStream = await ytdl(video.url, {
+        filter: 'audioonly',
+        requestOptions: {
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36'
+          }
+        }
+      });
 
       const result = {
         id: video.videoId,
         title: video.title,
         thumbnail: video.thumbnail,
-        audio: stream.url, // Link stream langsung
+        audio: audioStream, // stream audio dari @distube/ytdl-core dengan User-Agent
       };
 
       resolve(result);
