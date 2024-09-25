04f0c4a4875417e23fea50ea713539b78e02905b
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-25 15:20:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.get("/api/playmusic", async (req, res) => {
       return res.status(400).json({ error: 'Parameter "query" tidak ditemukan' });
     }
 
-    const result = await ptz.ytplay(query); // Panggil fungsi ytplay melalui ptz
+    const result = await ptz.playmusic(query); // Memanggil fungsi playmusic dari ptz
     res.status(200).json({
       status: 200,
       creator: "galihrhgnwn",
